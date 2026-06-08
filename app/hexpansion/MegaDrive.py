058c63e917566b12f2f95c9aa51f145d8730c101
Alex Rea <git@area.io>
2026-06-08 16:02:29 +0100

    Try to support six button MD Controllers

--- app/hexpansion/MegaDrive.py
+++ app/hexpansion/MegaDrive.py
@@ -1,18 +1,32 @@
 from .base import HexpansionModule, CommandStatus
 
 
+SIX_BUTTON_ONLY = {"x", "y", "z", "mode"}
+
+THREE_BUTTON_COMMANDS = ["start", "a", "up", "down", "left", "right", "b", "c"]
+SIX_BUTTON_COMMANDS = THREE_BUTTON_COMMANDS + ["x", "y", "z", "mode"]
+
+
 class MegaDriveModule(HexpansionModule):
     FRIENDLY_NAME = "MegaDrive"
-    COMMAND_OPTIONS = ["start", "a", "up", "down", "left", "right", "b", "c"]
+    COMMAND_OPTIONS = THREE_BUTTON_COMMANDS
 
     def __init__(self):
         super().__init__()
-        self.last_status = CommandStatus.WAITING
+        self.is_six_button = False
+
+    def reset(self):
+        super().reset()
+        self.is_six_button = False
+        self.COMMAND_OPTIONS = THREE_BUTTON_COMMANDS
 
     def on_button_down(self, event):
         button_name = self._get_button_name(event)
         if button_name is None:
             return
+        if button_name in SIX_BUTTON_ONLY and not self.is_six_button:
+            self.is_six_button = True
+            self.COMMAND_OPTIONS = SIX_BUTTON_COMMANDS
         if button_name == self.current_command:
             self.last_status = CommandStatus.PASSED
 
